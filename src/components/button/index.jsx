2ef7bbd890035b7fe4faaecc85f3f3de6d9afa73
Sam <samjameslaw@gmail.com>
2021-06-04 16:01:37 +0100

    Make button component

---
+++ src/components/button/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => (
+    <button type="button" id="button" onClick={() => alert('click!')}>
+        Click me
+    </button>
+);
+
+export default Button;
